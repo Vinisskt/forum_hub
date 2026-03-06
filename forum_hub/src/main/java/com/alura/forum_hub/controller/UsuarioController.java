eb368acebf22dca83b54267659b8d8dff0d99b9f
vinisskt <vinissktmaloca@gmail.com>
2026-03-06 02:54:07 -0300

    alterando rota uri adicionando / ao inicio da rota e passando id direto a rota uri

--- forum_hub/src/main/java/com/alura/forum_hub/controller/UsuarioController.java
+++ forum_hub/src/main/java/com/alura/forum_hub/controller/UsuarioController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alura.forum_hub.infra.DadosTokenAutenticacao;
 import com.alura.forum_hub.infra.TokenAutentificacao;
@@ -31,70 +32,72 @@ import jakarta.validation.Valid;
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-	@Autowired
-	private TokenAutentificacao tokenAutentificacao;
+  @Autowired
+  private TokenAutentificacao tokenAutentificacao;
 
-	@Autowired
-	@Lazy
-	private AuthenticationManager authenticationManager;
+  @Autowired
+  @Lazy
+  private AuthenticationManager authenticationManager;
 
-	@Autowired
-	private UsuarioRepository repository;
+  @Autowired
+  private UsuarioRepository repository;
 
-	@Autowired
-	private CadastroUsuarios cadastroUsuarios;
+  @Autowired
+  private CadastroUsuarios cadastroUsuarios;
 
-	@Autowired
-	private ValidacaoIdExiste idExiste;
+  @Autowired
+  private ValidacaoIdExiste idExiste;
 
-	@Autowired
-	private ValidacaoAtualizarUsuario validacaoAtualizarUsuario;
+  @Autowired
+  private ValidacaoAtualizarUsuario validacaoAtualizarUsuario;
 
-	@PostMapping("/cadastro")
-	@Transactional
-	public ResponseEntity cadastrarUsuario(@RequestBody @Valid DadosCadastroUsuario dados) {
-		var senha = cadastroUsuarios.gerarBcrypt(dados.senha());
-		var usuario = new Usuario(dados, senha);
-		repository.save(usuario);
-		return ResponseEntity.ok(new DadosDetalhamentoUsuario(usuario));
-	}
+  @PostMapping("/cadastro")
+  @Transactional
+  public ResponseEntity cadastrarUsuario(@RequestBody @Valid DadosCadastroUsuario dados,
+      UriComponentsBuilder uriComponentsBuilder) {
+    var senha = cadastroUsuarios.gerarBcrypt(dados.senha());
+    var usuario = new Usuario(dados, senha);
+    repository.save(usuario);
+    var uri = uriComponentsBuilder.path("/usuario/{id}").buildAndExpand(usuario.getId()).toUri();
+    return ResponseEntity.created(uri).body(new DadosDetalhamentoUsuario(usuario));
+  }
 
-	@PostMapping("/login")
-	public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoUsuario dados) {
-		var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-		var autenticacao = authenticationManager.authenticate(token);
-		var TokenJWT = tokenAutentificacao.gerarToken((Usuario) autenticacao.getPrincipal());
-		return ResponseEntity.ok(new DadosTokenAutenticacao(TokenJWT));
-	}
+  @PostMapping("/login")
+  public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoUsuario dados) {
+    var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+    var autenticacao = authenticationManager.authenticate(token);
+    var TokenJWT = tokenAutentificacao.gerarToken((Usuario) autenticacao.getPrincipal());
+    return ResponseEntity.ok(new DadosTokenAutenticacao(TokenJWT));
+  }
 
-	@GetMapping
-	public ResponseEntity<Page> listarUsuarios(@PageableDefault(size = 10, sort = { "login" }) Pageable page) {
-		var usuarios = repository.findAll(page).map(DadosDetalhamentoUsuario::new);
-		return ResponseEntity.ok(usuarios);
-	}
+  @GetMapping
+  public ResponseEntity<Page> listarUsuarios(@PageableDefault(size = 10, sort = { "login" }) Pageable page) {
+    var usuarios = repository.findAll(page).map(DadosDetalhamentoUsuario::new);
+    return ResponseEntity.ok(usuarios);
+  }
 
-	@GetMapping("/{id}")
-	public ResponseEntity detalharUsuario(@PathVariable Long id) {
-		idExiste.validarUsuario(id);
-		var detalhamento = repository.getReferenceById(id);
-		return ResponseEntity.ok(new DadosDetalhamentoUsuario(detalhamento));
-	}
+  @GetMapping("/{id}")
+  public ResponseEntity detalharUsuario(@PathVariable Long id) {
+    idExiste.validarUsuario(id);
+    var detalhamento = repository.getReferenceById(id);
+    return ResponseEntity.ok(new DadosDetalhamentoUsuario(detalhamento));
+  }
 
-	@PutMapping("/senha")
-	@Transactional
-	public ResponseEntity AtualizarSenha(@RequestBody @Valid DadosAtualizarUsuario dados) {
-		var usuario = validacaoAtualizarUsuario.validar(dados);
-		var senha = cadastroUsuarios.gerarBcrypt(dados.newSenha());
-		usuario.atualizar(senha);
-		return ResponseEntity.ok(new DadosDetalhamentoUsuario(usuario));
-	}
+  @PutMapping("/senha")
+  @Transactional
+  public ResponseEntity AtualizarSenha(@RequestBody @Valid DadosAtualizarUsuario dados) {
+    var usuario = validacaoAtualizarUsuario.validar(dados);
+    var senha = cadastroUsuarios.gerarBcrypt(dados.newSenha());
+    usuario.atualizar(senha);
+    return ResponseEntity.ok(new DadosDetalhamentoUsuario(usuario));
+  }
 
-	@DeleteMapping("/{id}")
-	@Transactional
-	public ResponseEntity deletarUsuario(@PathVariable Long id) {
-		idExiste.validarUsuario(id);
-		repository.deleteById(id);
-		return ResponseEntity.noContent().build();
+  @DeleteMapping("/{id}")
+  @Transactional
+  public ResponseEntity deletarUsuario(@PathVariable Long id) {
+    idExiste.validarUsuario(id);
+    repository.deleteById(id);
+    return ResponseEntity.noContent().build();
 
-	}
+  }
 }
