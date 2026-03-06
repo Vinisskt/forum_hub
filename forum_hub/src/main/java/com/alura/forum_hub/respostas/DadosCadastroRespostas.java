6c27293e7dfb8d3b84ba3dc75ca7641868ccd7ca
vinisskt <vinissktmaloca@gmail.com>
2026-03-06 02:55:12 -0300

    retirando usuario por ver que nao e necessario, porque ja e passado verificado pelo usuario logado

--- forum_hub/src/main/java/com/alura/forum_hub/respostas/DadosCadastroRespostas.java
+++ forum_hub/src/main/java/com/alura/forum_hub/respostas/DadosCadastroRespostas.java
@@ -1,9 +1,6 @@
 package com.alura.forum_hub.respostas;
 
-import com.alura.forum_hub.usuario.Usuario;
-
 public record DadosCadastroRespostas(
-		Usuario usuario,
-		String resposta) {
+    String resposta) {
 
 }
